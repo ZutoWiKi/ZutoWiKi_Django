ad94c9c951c764a4641c33749584730d8ef50a8c
이형주 <jujumom630@gmail.com>
2025-07-08 18:48:13 +0900

    3줄 GOAT 문제 해결
    이미지 미리보기까지 완벽

--- ZutoSite/urls.py
+++ ZutoSite/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("user/", include("User.urls")),
     path("post/", include("ZutoPages.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
